30dedad13568c33620f61b866cb9d785e7f6bef1
VincentFourcroy <vincentfourcroy@hotmail.fr>
2024-04-14 23:51:26 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  app.enableCors()
+
+  const config = new DocumentBuilder()
+    .setTitle('ShelfDiver-API')
+    .setDescription('Back-end API for the ShelfDiver app.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(process.env.API_PORT)
+}
+bootstrap()
